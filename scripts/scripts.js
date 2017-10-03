5a576e4dd981f515f7dd9bb767930378b732cce3
Dustin Hammack <rdhammack88@hotmail.com>
2017-10-03 03:55:48 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,22 +10,22 @@ window.onload = function() {
 			if( checkboxValues[i].checked ) {
 				switch(checkboxValues[i].value) {
 					case 'months':
-						document.getElementById('months').innerHTML = "Your age in months is " +  (age * 12) + "<br/>";
+						document.getElementById('months').innerHTML = "Your age in months is " +  Math.floor(age * 12) + "<br/>";
 						break;
 					case 'weeks':
-						document.getElementById('weeks').innerHTML = "Your age in weeks is " + (age * 52) + "<br/>";
+						document.getElementById('weeks').innerHTML = "Your age in weeks is " + Math.floor(age * 52) + "<br/>";
 						break;
 					case 'days':
-						document.getElementById('days').innerHTML = "Your age in days is " + ((age * 365) - (Math.floor(age/4))) + "<br/>";
+						document.getElementById('days').innerHTML = "Your age in days is " + Math.floor((age * 365) - (Math.floor(age/4))) + "<br/>";
 						break;
 					case 'hours':
-						document.getElementById('hours').innerHTML = "Your age in hours is " + (((age * 365) - (Math.floor(age/4))) * 24) + "<br/>";
+						document.getElementById('hours').innerHTML = "Your age in hours is " + Math.floor(((age * 365) - (Math.floor(age/4))) * 24) + "<br/>";
 						break;
 					case 'minutes':
-						document.getElementById('minutes').innerHTML = "Your age in minutes is " + (((age * 365) - (Math.floor(age/4))) * 24 * 60) + "<br/>";
+						document.getElementById('minutes').innerHTML = "Your age in minutes is " + Math.floor(((age * 365) - (Math.floor(age/4))) * 24 * 60) + "<br/>";
 						break;
 					case 'seconds':
-						document.getElementById('seconds').innerHTML = "Your age in seconds is " + (((age * 365) - (Math.floor(age/4))) * 24 * 60 * 60) + "<br/>";
+						document.getElementById('seconds').innerHTML = "Your age in seconds is " + Math.floor(((age * 365) - (Math.floor(age/4))) * 24 * 60 * 60) + "<br/>";
 						break;
 				}	
 			} else if( !checkboxValues[i].checked ) {
