d0cb89e782c86ea77f25fd0608ffc9aa70eba8f2
Dustin Hammack <rdhammack88@hotmail.com>
2017-10-24 15:46:28 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,26 @@
 window.onload = function() {
-
+	// Calculate button
 	var calculateBtn = document.getElementById('calculate');
 	
 	function calculateAge() {
-		var age = document.getElementById('age').value;		
+		var age = parseInt(document.getElementById('age').value);
 		var checkboxValues = document.querySelectorAll('input[type="checkbox"]');
+		var errorDiv = document.getElementById('hiddenError');
+		var errorMessage = document.getElementById('errorMessage');
+		var checkedboxes = [];
+		console.log(checkboxValues.checked);
 		
+		// Verify age has a value, and that it is greater than 0
+		if( !age || age <= 0 ) { 
+			errorMessage.innerHTML = 'Please enter a valid age to calculate';
+			errorDiv.classList.remove('hidden');
+			return; 
+		} // End of age check conditional
+				
 		for( var i = 0; i < checkboxValues.length; i++ ) {
+			errorDiv.classList.add('hidden');
 			if( checkboxValues[i].checked ) {
+				checkedboxes.push(checkboxValues[i].value);
 				switch(checkboxValues[i].value) {
 					case 'months':
 						document.getElementById('months').innerHTML = "Your age in months is " +  Math.floor(age * 12) + "<br/>";
@@ -27,7 +40,7 @@ window.onload = function() {
 					case 'seconds':
 						document.getElementById('seconds').innerHTML = "Your age in seconds is " + Math.floor(((age * 365) - (Math.floor(age/4))) * 24 * 60 * 60) + "<br/>";
 						break;
-				}	
+				} // End of switch statement
 			} else if( !checkboxValues[i].checked ) {
 				switch(checkboxValues[i].value) {
 					case 'months':
@@ -48,10 +61,20 @@ window.onload = function() {
 					case 'seconds':
 						document.getElementById('seconds').innerHTML = "";
 						break;
-				}	
-			}
+				} // End of switch statement	
+			} // End of else if statement
+		} // End of for loop
+		
+		console.log(checkedboxes.length);
+		if( checkedboxes.length <= 0 ) { 
+			errorMessage.innerHTML = 'Please check at least one format to calculate your age with';
+			errorDiv.classList.remove('hidden');
+			return; 
+		} else {
+			errorDiv.classList.add('hidden');
 		}
-	}
+	} // End of calculateAge() function
 	
+	// Add event listener for when Calculate button is clicked
 	calculateBtn.addEventListener('click', calculateAge);
 }
